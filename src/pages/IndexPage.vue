f37661b55fd63b7649a956c53729fe25e31cda10
aestas <hawhawba@gmail.com>
2022-06-06 19:53:48 +0900

    update: first test

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,32 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    {{ title }}
+    <test-card class="green q-ma-lg" />
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import TestCard from 'src/components/TestCard.vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { TestCard },
+  data() {
+    return {
+      title: '커퍼즈 Cuppers'
+    }
+  },
+  mounted() {
+    let apiUrl = 'http://localhost:3000/posts'
+    this.$axios
+      .get(apiUrl)
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 })
 </script>
