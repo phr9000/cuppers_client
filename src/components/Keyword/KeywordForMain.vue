6bfc4ed7b03ed3410d6066ce3e66dcbab2084c93
aestas <hawhawba@gmail.com>
2022-06-11 01:54:15 +0900

    [merge] a8754b2 커밋까지 병합

---
+++ src/components/Keyword/KeywordForMain.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- <div class="keyword-item" v-for="(a, i) in CafeKeywordsData" :key="i"> -->
+  <a href="#" class="keyword-link">{{ keyword }}</a>
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  name: 'KeywordForMain',
+  props: {
+    keyword: {
+      type: String,
+      default: '키워드'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.keyword {
+  text-align: center;
+}
+h4 {
+  margin-bottom: 15px;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+.keyword-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+.keyword-item {
+  width: 100px;
+  height: 100px;
+  // border: 1px solid #b5afaf;
+  // margin: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // background-color: rgb(251, 251, 251);
+  transition: 0.5s;
+}
+.keyword-item:hover {
+  /* flex-grow: 1.5; */
+  background: rgb(235, 235, 235);
+  width: 105px;
+  // height: 110px;
+}
+.keyword-link {
+  font-size: 10px;
+  // font-weight: bold;
+  color: #555;
+  text-decoration: none;
+  text-align: center;
+}
+.keyword-link:hover {
+  // color: white;
+}
+</style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,33 +1,66 @@
 <template>
-  <h3>따뜻한<br />커피 한 잔이<br />좋다</h3>
-  <CNoteCarousel />
-  <CafeKeywords :CafeKeywordsData="CafeKeywordsData" />
-  <RecommendList />
-  <div class="">
-    <div class="q-pa-md q-gutter-sm" style="padding-top: 850px">
-      <q-btn
-        color="black"
-        label="지도 표시하기"
-        icon="map"
-        @click="MapRedirect()"
-      ></q-btn>
+  <q-page>
+    <div class="column">
+      <div class="q-pl-xl col">
+        <h3 class="q-mb-lg">따뜻한<br />커피 한 잔이<br />좋다☕</h3>
+      </div>
     </div>
-  </div>
+    <CNoteCarousel />
+
+    <!-- 카페 키워드 -->
+
+    <div class="keyword-container column constrain">
+      <div class="keyword">
+        <h4>CAFE KEYWORD</h4>
+        <p>
+          <span class="txt_keyword"> 키워드로 분류된 다양한 카페 모음 </span>
+        </p>
+      </div>
+      <div class="column">
+        <div class="row">
+          <div class="col-2"></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-1"><keyword-for-main /></div>
+          <div class="col-2"></div>
+        </div>
+      </div>
+
+      <!-- <RecommendList /> -->
+      <div class="">
+        <div class="q-pa-md q-gutter-sm" style="padding-top: 850px">
+          <q-btn
+            color="black"
+            label="지도 표시하기"
+            icon="map"
+            @click="MapRedirect()"
+          ></q-btn>
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
-import RecommendList from '../components/Card/RecommendList.vue'
-import CNoteCarousel from '../components/Carousel/CNoteCarousel.vue'
-import CafeKeywords from '../components/Keyword/CafeKeywords.vue'
-
 import CafeKeywordsData from '../data/CafeKeywordsData'
+
+// import RecommendList from '../components/Card/RecommendList.vue'
+import CNoteCarousel from '../components/Carousel/CNoteCarousel.vue'
+// import CafeKeywords from '../components/Keyword/CafeKeywords.vue'
+import KeywordForMain from '../components/Keyword/KeywordForMain.vue'
 
 export default {
   name: 'MainPage',
   components: {
     CNoteCarousel,
-    CafeKeywords,
-    RecommendList
+    KeywordForMain
+    // CafeKeywords
+    // RecommendList
   },
   data() {
     return {
@@ -42,8 +75,8 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-h3 {
-  padding-left: 30px;
+.keyword-container {
+  text-align: center;
 }
 .q-pa-md q-gutter-sm {
   display: block;
