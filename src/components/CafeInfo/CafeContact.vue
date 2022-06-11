14c929a0833a8d8464ce0f902a5eb204854d1181
Resister-boy <hassanpumped17@gmail.com>
2022-06-11 15:07:51 +0900

    update CafeInformation

--- src/components/CafeInfo/CafeContact.vue
+++ src/components/CafeInfo/CafeContact.vue
@@ -1,26 +1,36 @@
 <template>
   <!-- 카페 연락처(전화번호, 홈페이지, 인스타그램) -->
   <div>
-    <p>{{ CafeContactData[0].tel }}</p>
-    <a href="">{{ CafeContactData[0].web }}</a>
+    <p>{{ firstContact.tel }}</p>
+    <q-icon :name="language" /><a href="">{{ firstContact.web }}</a>
     <br />
-    <a href="">{{ CafeContactData[0].insta }}</a>
+    <a href="">{{ firstContact.insta }}</a>
   </div>
 </template>
 
 <script>
+import { language } from '@quasar/extras/material-icons'
+import CafeContactData from '../../data/CafeContactData'
+
 export default {
   name: 'CafeContact',
   props: {
     CafeContactData: Array
+  },
+  created() {
+    this.language = language
+  },
+  data() {
+    return {
+      CafeContact: CafeContactData
+    }
+  },
+  computed: {
+    firstContact() {
+      return this.CafeContact[0]
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-a,
-p {
-  padding-left: 10px;
-  margin-bottom: 5px;
-}
-</style>
+<style lang="scss" scoped></style>
