debed445263b47538b477dc865d6b090f2775ad7
Resister-boy <hassanpumped17@gmail.com>
2022-06-11 16:36:36 +0900

    delete icon

--- src/components/CafeInfo/CafeContact.vue
+++ src/components/CafeInfo/CafeContact.vue
@@ -2,23 +2,19 @@
   <!-- 카페 연락처(전화번호, 홈페이지, 인스타그램) -->
   <div>
     <p>{{ firstContact.tel }}</p>
-    <q-icon :name="language" /><a href="">{{ firstContact.web }}</a>
+    <a href="">{{ firstContact.web }}</a>
     <br />
     <a href="">{{ firstContact.insta }}</a>
   </div>
 </template>
 
 <script>
-import { language } from '@quasar/extras/material-icons'
 import CafeContactData from '../../data/CafeContactData'
 
 export default {
   name: 'CafeContact',
   props: {
     CafeContactData: Array
-  },
-  created() {
-    this.language = language
   },
   data() {
     return {
