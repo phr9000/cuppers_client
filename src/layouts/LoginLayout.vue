eab70555f62fddcf641a949bd2d85750200df40e
JUHA <hanjuhaa@gmail.com>
2022-06-17 02:47:16 +0900

    add: 회원가입 서베이

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="hHh Lpr fFf">
+    <header-global />
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import HeaderGlobal from 'src/components/Global/HeaderGlobal.vue'
+
+export default defineComponent({
+  name: 'LoginLayout',
+  components: {
+    HeaderGlobal
+  }
+})
+</script>
