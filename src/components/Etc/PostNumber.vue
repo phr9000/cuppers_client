12b8e3c24b988fc2a83eb9ab64576d2a96d0cf2d
Resister-boy <hassanpumped17@gmail.com>
2022-06-19 21:06:35 +0900

    update addCafePage

--- src/components/Etc/PostNumber.vue
+++ src/components/Etc/PostNumber.vue
@@ -1,46 +1,12 @@
 <template>
-  <div>
-    <div class="row">
-      <q-input
-        type="text"
-        id="address"
-        label="주소"
-        v-model="address"
-        class="col-9"
-        :rules="[(val) => !!val || 'Field is required']"
-      />
-      <BtnBasic
-        type="button"
-        label="우편번호 찾기"
-        @click="execDaumPostcode()"
-        color="primary"
-        class="col-2 q-ml-xl q-my-md"
-      />
-    </div>
-    <div class="row">
-      <q-input
-        type="text"
-        id="detailAddress"
-        v-model="detailAddress"
-        label="상세주소"
-        class="col-9 q-mr-xl"
-        :rules="[(val) => !!val || 'Field is required']"
-      />
-      <q-input
-        type="text"
-        v-model="postcode"
-        label="우편번호"
-        class="col-2 q-ml-sm"
-        :rules="[(val) => !!val || 'Field is required']"
-      />
-    </div>
-    <q-input
-      type="text"
-      v-model="extraAddress"
-      id="extraAddress"
-      label="참고사항"
-    />
-  </div>
+  <BtnBasic
+    size="xs"
+    type="button"
+    label="주소 찾기"
+    @click="execDaumPostcode()"
+    color="primary"
+    class="text-caption"
+  />
 </template>
 
 <script>
@@ -52,26 +18,27 @@ export default {
   },
   data() {
     return {
-      address: '',
-      detailAddress: '',
-      extraAddress: '',
-      postcode: ''
+      cafe_address_info: {
+        address: this.address,
+        detailAddress: this.address,
+        extraAddress: this.address,
+        postcode: this.postcode
+      }
     }
   },
-  computed: {},
   methods: {
     execDaumPostcode() {
       new window.daum.Postcode({
         oncomplete: (data) => {
-          if (this.extraAddress !== '') {
-            this.extraAddress = ''
+          if (this.cafe_address_info.extraAddress !== '') {
+            this.cafe_address_info.extraAddress = ''
           }
           if (data.userSelectedType === 'R') {
             // 사용자가 도로명 주소를 선택했을 경우
-            this.address = data.roadAddress
+            this.cafe_address_info.address = data.roadAddress
           } else {
             // 사용자가 지번 주소를 선택했을 경우(J)
-            this.address = data.jibunAddress
+            this.cafe_address_info.address = data.jibunAddress
           }
 
           // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
@@ -79,36 +46,31 @@ export default {
             // 법정동명이 있을 경우 추가한다. (법정리는 제외)
             // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
             if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-              this.extraAddress += data.bname
+              this.cafe_address_info.extraAddress += data.bname
             }
             // 건물명이 있고, 공동주택일 경우 추가한다.
             if (data.buildingName !== '' && data.apartment === 'Y') {
-              this.extraAddress +=
-                this.extraAddress !== ''
+              this.cafe_address_info.extraAddress +=
+                this.cafe_address_info.extraAddress !== ''
                   ? `, ${data.buildingName}`
                   : data.buildingName
             }
             // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-            if (this.extraAddress !== '') {
-              this.extraAddress = `(${this.extraAddress})`
+            if (this.cafe_address_info.extraAddress !== '') {
+              this.cafe_address_info.extraAddress = `(${this.cafe_address_info.extraAddress})`
             }
           } else {
-            this.extraAddress = ''
+            this.cafe_address_info.extraAddress = ''
           }
           // 우편번호를 입력한다.
-          this.postcode = data.zonecode
+          this.cafe_address_info.postcode = data.zonecode
         }
       }).open()
     },
     sendData() {
-      this.$emit(
-        'sendPostData',
-        this.address,
-        this.detailAddress,
-        this.extraAddress,
-        this.postcode
-      )
+      this.$emit('sendPostData', this.cafe_address_info)
     }
   }
 }
 </script>
+<style lang="scss" scoped></style>
--- src/pages/AddNewCafePage.vue
+++ src/pages/AddNewCafePage.vue
@@ -1,82 +1,99 @@
 <template>
-  <q-page class="constrain">
-    <section class="q-py-md section">
-      <div class="text-center" style="font-size: 30px">
+  <q-page class="constrain_sm">
+    <section class="q-py-md">
+      <div class="text-center text-h5">
         <span>카페정보</span>
       </div>
     </section>
-    <section class="q-mx-xl">
-      <div class="text-right q-mr-xl q-mt-xl">
-        <q-icon
-          size="xs"
-          name="emergency"
-          style="color: #ff8a00"
-          class="q-mr-xs"
-        /><span>는 필수항목 입니다</span>
+    <section class="q-mx-xl q-my-xl q-pb-xl">
+      <div class="text-right q-mr-xl q-mt-xl cafe-notice">
+        <q-icon size="11px" name="emergency" class="q-mr-xs asterisk" /><span
+          >는 필수항목 입니다</span
+        >
       </div>
-      <!-- 카페 이름 -->
-      <div class="q-mx-xl q-pl-xl q-mt-xl row">
-        <div class="col-2 text-left q-my-auto">
-          <span style="font-size: 1.5rem">카페이름</span>
-          <q-icon size="12px" name="emergency" style="color: #ff8a00" />
+      <div class="bg-amber-1 q-py-sm q-mt-md background">
+        <!-- 카페 이름 -->
+        <div class="col-12 q-mx-xl q-my-xl contents_block">
+          <div class="row info-block">
+            <q-input
+              label="카페이름 *"
+              v-model="cafe_info.cafe_name_pr"
+              type="text"
+              :rules="[(val) => !!val || 'Field is required']"
+              class="col-8 input-area"
+            />
+            <div class="col-3 q-my-auto row-area">
+              <BtnBasic
+                type="button"
+                label="중복 확인"
+                size="md"
+                color="primary"
+                padding="7px 20px"
+                class="button"
+              />
+            </div>
+          </div>
         </div>
-        <div class="col-10">
-          <q-input
-            label="카페이름"
-            v-model="cafe_name_pr"
-            type="text"
-            :dense="dense"
-            :rules="[(val) => !!val || 'Field is required']"
-          />
+        <div class="col-12 q-mx-xl q-mt-xl contents_block">
+          <div class="row info-block">
+            <q-input
+              type="text"
+              id="address"
+              label="주소 *"
+              class="col-8 input-area"
+              :rules="[(val) => !!val || 'Field is required']"
+            />
+            <div class="col-3 q-my-auto flex row-area">
+              <PostNumber
+                size="md"
+                ref="PostNumber"
+                @sendPostData="getPostData"
+                padding="7px 20px"
+                class="button"
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="q-mx-xl q-pl-xl q-mt-xl row">
-        <div class="col-2 text-left q-my-auto">
-          <span class="q-pr-xl" style="font-size: 1.5rem">주</span>
-          <span style="font-size: 1.5rem">소</span>
-          <q-icon size="12px" name="emergency" style="color: #ff8a00" />
+        <div class="col-12 q-mx-xl q-mb-xl contents_block">
+          <div class="row info-block">
+            <div class="col-7">
+              <q-input
+                type="text"
+                id="detailAddress"
+                label="상세주소 *"
+                :rules="[(val) => !!val || 'Field is required']"
+              />
+            </div>
+            <div class="col-4 q-ml-xs">
+              <q-input
+                type="text"
+                label="우편번호 *"
+                :rules="[(val) => !!val || 'Field is required']"
+              />
+            </div>
+          </div>
         </div>
-        <PostNumber
-          ref="PostNumber"
-          @sendPostData="getPostData"
-          class="col-10"
-        />
-      </div>
-      <!-- 카페 전화번호-->
-      <div class="q-mx-xl q-pl-xl q-mt-xl row">
-        <div class="col-2 text-left q-my-auto">
-          <span style="font-size: 1.5rem">대표전화</span>
-          <q-icon size="12px" name="emergency" style="color: #ff8a00" />
+        <!-- 카페 전화번호-->
+        <div class="q-mx-xl q-mt-xl q-my-xl contents_block">
+          <div class="col-12">
+            <q-input
+              label="대표전화 *"
+              v-model="cafe_info.cafe_phone"
+              type="tel"
+              mask="### -  #### - ####"
+              :rules="[(val) => !!val || 'Field is required']"
+            />
+          </div>
         </div>
-        <div class="col-10">
-          <q-input
-            label="대표전화"
-            v-model="cafe_phone"
-            type="tel"
-            :dense="dense"
-            mask="### -  #### - ####"
-            :rules="[(val) => !!val || 'Field is required']"
-          />
+        <div class="q-mx-xl q-mt-xl q-my-xl contents_block">
+          <div class="col-12">
+            <q-input label="영업시간" v-model="cafe_info.cafe_operation_time" />
+          </div>
         </div>
-      </div>
-      <div class="q-mx-xl q-pl-xl q-mt-xl row">
-        <div class="col-2 text-left q-my-auto">
-          <span style="font-size: 1.5rem">영업시간</span>
-        </div>
-        <div class="col-10">
-          <q-input
-            label="영업시간"
-            v-model="cafe_operation_time"
-            :dense="dense"
-          />
-        </div>
-      </div>
-      <div class="q-mx-xl q-pl-xl q-mt-xl row">
-        <div class="col-2 text-left q-mt-auto">
-          <span style="font-size: 1.5rem">홈페이지</span>
-        </div>
-        <div class="col-10">
-          <q-input v-model="cafe_website" :dense="dense" />
+        <div class="q-mx-xl q-mt-xl q-my-xl contents_block">
+          <div class="col-12">
+            <q-input label="웹사이트" v-model="cafe_info.cafe_website" />
+          </div>
         </div>
       </div>
     </section>
@@ -85,7 +102,7 @@
         <BtnBasic
           @click="show"
           color="primary"
-          size="md"
+          size="sm"
           label="카페 등록하기"
           icon="note"
           padding="10px 30px 10px 30px"
@@ -107,32 +124,54 @@ export default defineComponent({
   },
   data() {
     return {
-      cafe_name_pr: this.cafe_name_pr,
-      cafe_phone: this.cafe_phone,
-      cafe_operation_time: this.cafe_operation_time,
-      cafe_website: this.cafe_website
+      cafe_info: {
+        cafe_name_pr: this.cafe_name_pr,
+        cafe_phone: this.cafe_phone,
+        cafe_operation_time: this.cafe_operation_time,
+        cafe_website: this.cafe_website
+      }
+    }
+  },
+  computed: {
+    addressData(cafe_address_info) {
+      return cafe_address_info.address
     }
   },
   methods: {
     show() {
-      console.log('카페이름: ', this.cafe_name_pr)
-      console.log('전화번호: ', this.cafe_phone)
-      console.log('운영시간: ', this.cafe_operation_time)
-      console.log('홈페이지: ', this.cafe_website)
+      console.log('카페정보: ', this.cafe_info)
       this.$refs.PostNumber.sendData()
     },
-    getPostData(address, detailAddress, extraAddress, postcode) {
-      console.log('카페주소: ', address)
-      console.log('상세주소: ', detailAddress)
-      console.log('참고사항: ', extraAddress)
-      console.log('우편번호: ', postcode)
+    getPostData(cafe_address_info) {
+      console.log('카페주소: ', cafe_address_info)
     }
-  },
-  mounted() {}
+  }
 })
 </script>
 <style lang="scss" scoped>
-.section {
-  border-bottom: 3px solid #333;
+.asterisk {
+  color: rgb(255, 136, 0);
+}
+
+.background {
+  outline: 1px solid #ccc;
+  box-sizing: content-box;
+  border-radius: 15px;
+  .info-block {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+
+@media (max-width: 800px) {
+  .cafe-notice {
+    font-size: 0.8rem;
+  }
+  .contents_block {
+    margin: 48px 15px;
+    .button {
+      size: 0.5rem;
+    }
+  }
 }
 </style>
