8122ede0edd19ed01cb4540bdbdffb9c1ca11500
Aestas <hawhawba@gmail.com>
2022-06-22 00:39:12 +0900

    [add] BtnReview

---
+++ src/components/Button/BtnReview.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-btn
+    @click="handleCLick"
+    padding="2px 8px"
+    flat
+    dense
+    rounded
+    color="primary"
+    class="btn row no-wrap items-center"
+  >
+    <q-icon size="xs" class="icon_fav" name="notes" />
+    <span class="review_cnt q-ml-xs text-h6"> {{ reviewCnt }}</span>
+  </q-btn>
+</template>
+<script>
+export default {
+  name: 'BtnReview',
+  components: {},
+  props: {
+    cafe_id: { type: Number, required: true },
+    review_cnt: {
+      type: Number,
+      required: true
+    } // 이 '~'의 좋아요 총 합
+  },
+  data() {
+    return {
+      reviewCnt: 0,
+      icon: 'favorite_border'
+    }
+  },
+  mounted() {
+    this.reviewCnt = this.review_cnt
+  },
+  methods: {
+    handleCLick() {
+      console.log('click')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.block {
+  font-size: 4rem !important;
+}
+.btn {
+  cursor: pointer;
+
+  .icon_fav {
+    color: $grey-5;
+    &.colored {
+      color: $primary;
+    }
+  }
+  .review_cnt {
+    line-height: 18px;
+    color: $grey-5;
+    font-size: 18px;
+    &.colored {
+      color: $primary;
+    }
+  }
+
+  &:hover {
+    .icon_fav {
+      transition: all 0.3s;
+      color: $primary;
+    }
+    .review_cnt {
+      line-height: 18px;
+      transition: all 0.3s;
+      color: $primary;
+    }
+  }
+}
+</style>
---
+++ src/pages/test/CardTest2.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="q-pa-sm">
+    <!-- CardCafeMap 카페카드(지도화면) -->
+    <div class="q-ma-xs q-pa-xs custom_test radius_border">
+      <p>card-cafe-map 카페카드(지도화면)</p>
+      <div class="row">
+        <div class="q-mr-sm" style="width: 500px">
+          <card-cafe-map @close="handleClose" />
+        </div>
+        <div class="q-mr-sm" style="width: 400px">
+          <card-cafe-map @close="handleClose" />
+        </div>
+        <div style="width: 300px"><card-cafe-map @close="handleClose" /></div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import CardCafeMap from 'src/components/Card/CardCafeMap.vue'
+
+export default defineComponent({
+  components: { CardCafeMap },
+  data() {
+    return {}
+  },
+  methods: {
+    handleClose() {}
+  }
+})
+</script>
