54eea8c3856809758ccbb7932e0e4e081b926432
studentinbb <75927514+studentinbb@users.noreply.github.com>
2022-07-01 21:42:41 +0800

    [Update]Upload images
    서버에 이미지 넘기고, 주소 받아오는 코드

--- src/pages/test/CnoteDetailTest2.vue
+++ src/pages/test/CnoteDetailTest2.vue
@@ -4,6 +4,9 @@
   <div class="btn-box">
     <button class="send-btn" @click="sendCnote()">등록하기</button>
   </div>
+  <div class="btn-box">
+    <button class="send-btn" @click="getImage()">images보기</button>
+  </div>
   <!-- 에디터 내용을 받을 곳  -->
   <div id="contents" @change="onChange()"></div>
 </template>
@@ -20,7 +23,8 @@ export default {
   components: {},
   data() {
     return {
-      editor: null
+      editor: null,
+      images: []
     }
   },
   mounted() {
@@ -32,11 +36,27 @@ export default {
       previewStyle: 'tab',
       viewer: true,
       hooks: {
-        addImageBlobHook: (blob, callback) => {
-          this.onUploadImage(blob).then((res) => {
-            const path = res.path
-            callback(path, 'img')
-          })
+        addImageBlobHook: async (blob, callback) => {
+          console.log(blob)
+          const formData = new FormData()
+          const url = 'http://localhost:3000/static/images/'
+          formData.append('image', blob)
+          await this.$axios
+            .post('http://localhost:3000/api/upload/image', formData, {
+              headers: { 'Content-Type': 'multipart/form-data' }
+            })
+            .then((r) => {
+              console.log(r)
+              console.log(r.data.path)
+              this.images.push(r.data.path)
+              callback(url + r.data.filename, 'alt text')
+            })
+            .catch((e) => {
+              console.log(e)
+            })
+          // console.log(res.path)
+
+          return false
         }
       }
     })
@@ -44,6 +64,26 @@ export default {
   methods: {
     sendCnote() {
       console.log(this.editor.getHTML())
+    },
+    addImageBlobHook: (blob, callback) => {
+      const onUploadImage = async (blob, callback) => {
+        console.log(blob)
+        const formData = new FormData()
+        formData.append('image', blob)
+        const res = await axios
+          .post('http://localhost:3000/api/upload/image', formData, {
+            headers: { 'Content-Type': 'multipart/form-data' }
+          })
+          .catch((e) => {
+            console.log(e)
+          })
+        conole.log(res)
+        callback(res.path, 'alt text')
+        return false
+      }
+    },
+    getImage() {
+      console.log(this.images)
     }
   }
 }
