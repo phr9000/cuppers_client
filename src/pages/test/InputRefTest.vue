65be3b8cdfba5868fa229a33d0c36f3904926f6f
Aestas <hawhawba@gmail.com>
2022-07-02 14:45:14 +0900

    [cleanup] del others

--- src/pages/test/InputRefTest.vue
+++ src/pages/test/InputRefTest.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <q-btn @click="changeVal" label="change Value" />
-    <child :menu_id="1" :menu_type="1" ref="child" />
-
-    <q-separator class="q-my-xl"></q-separator>
-
-    <q-btn @click="changeArrayVal" label="change Array Value" />
-    <child v-for="arr in array" :key="arr.id" ref="childOfArray" />
-
-    <q-separator class="q-my-xl"></q-separator>
-
     <q-btn @click="addInput" label="add Input" />
     <child-input
       v-for="item in inputs"
@@ -17,31 +7,18 @@
       :id="item.id"
       ref="childOfInputs"
     />
-
-    <br />
     <q-btn @click="getAllInputValues" label="get all Input Values" />
-    <q-separator class="q-my-xl"></q-separator>
   </div>
 </template>
 <script>
-import Child from 'src/pages/test/child/ChildComp.vue'
 import ChildInput from 'src/pages/test/child/ChildInput.vue'
 export default {
   name: 'InputRefTest',
   components: {
-    Child,
     ChildInput
   },
   data() {
     return {
-      array: [
-        {
-          id: 1
-        },
-        {
-          id: 2
-        }
-      ],
       inputs: [
         {
           id: 1
@@ -50,15 +27,6 @@ export default {
     }
   },
   methods: {
-    changeVal() {
-      this.$refs.child.changeVal()
-    },
-    changeArrayVal() {
-      for (let i = 0; i < this.array.length; i++) {
-        this.$refs.childOfArray[i].changeVal()
-        // this.array.push()
-      }
-    },
     addInput() {
       const id = this.inputs.length + 1
       this.inputs.push({
