edf73a5ea9c0ae14b0acea5d0644f1daa6b8d294
Aestas <hawhawba@gmail.com>
2022-07-08 20:37:02 +0900

    [add] 지도 마이리스트 작업 json-server 연동하여 1차 구현

---
+++ src/components/List/MapMyList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <q-item-label header> 마이리스트 </q-item-label>
+    <q-list bordered>
+      <q-item
+        @click="clickItem(item)"
+        v-for="item in items"
+        :key="item.mylist_id"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>{{ item.mylist_name }}</q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapMyList',
+  components: {
+    // BtnBasic
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => {
+        return null
+      }
+    }
+  },
+  methods: {
+    clickItem(item) {
+      // console.log(mylist_id)
+      this.$emit('click_item', item)
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/Tab/TabSort.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-tabs
+    active-color="secondary"
+    v-model="current"
+    dense
+    class="sort_tabs text-grey-6"
+  >
+    <q-tab
+      v-for="item in sort_items"
+      :key="item.val"
+      class="tab"
+      :name="item.val"
+      :label="item.label"
+    />
+  </q-tabs>
+</template>
+
+<script>
+export default {
+  name: 'TabSort',
+  components: {},
+  props: {
+    sort_items: {
+      type: Array,
+      default: () => {
+        return null
+      }
+    }
+  },
+  data() {
+    return {
+      current: ''
+    }
+  },
+  watch: {
+    current(val) {
+      this.$emit('change', val)
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
