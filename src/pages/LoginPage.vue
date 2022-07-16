0365a3a5064d454aa4bfeb7934f2ddc6a5a0b335
hailan <78142246+phr9000@users.noreply.github.com>
2022-07-16 10:25:13 +0900

    [~ing]kakao login api test

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -8,6 +8,8 @@
       <div><button @click="login">입력한 uid로 로그인</button></div>
       <div v-if="user">logged user: {{ user }}</div>
       <button @click="kakaoLogin">카카오로 로그인 하기</button>
+      <button @click="kakaoLogout">카카오로그아웃</button>
+      <button @click="kakaoOut">카카오탈퇴</button>
     </div>
   </q-page>
 </template>
@@ -46,6 +48,54 @@ export default defineComponent({
   },
   created() {},
   methods: {
+    kakaoLogin() {
+      window.Kakao.API.request({
+        url: '/v2/user/me',
+        success: (res) => {
+          const kakao_account = res.kakao_account
+          console.log(kakao_account)
+          var accessToken = Kakao.Auth.getAccessToken() // 액세스 토큰 할당
+          console.log(accessToken)
+        }
+      })
+    },
+    // 로그아웃
+    kakaoLogout() {
+      Kakao.init(process.env.KAKAO_API)
+      Kakao.isInitialized()
+      if (!Kakao.Auth.getAccessToken()) {
+        // 토큰이 있는지 확인 (토큰 가져와보기)
+        console.log('Not logged in.')
+        return
+      }
+
+      Kakao.Auth.logout(function () {
+        // 카카오 로그아웃
+        console.log(Kakao.Auth.getAccessToken())
+      })
+    },
+    // 탈퇴
+    kakaoOut() {
+      Kakao.init(process.env.KAKAO_API)
+      Kakao.isInitialized()
+      Kakao.Auth.setAccessToken(
+        QZigipQ81eBBhic4LGFlrRKDjQxgq8gZ7dFNhBIfCj10lwAAAYIEIhgM
+      )
+
+      Kakao.API.request({
+        url: '/v1/user/unlink',
+        success: function (response) {
+          console.log(response)
+          callbackFunction() // 연결끊기(탈퇴)성공시 서버에서 처리할 함수
+        },
+        fail: function (error) {
+          alert(
+            '탈퇴처리가 미완료되었습니다. \n관리자에게 문의하시기 바랍니다.'
+          )
+          console.log(error)
+        }
+      })
+    },
     login() {
       const uid = parseInt(this.uid)
       this.user = {
