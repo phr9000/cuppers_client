bc784b929eeb026916384fbf5444b9c2992cfc16
Aestas <hawhawba@gmail.com>
2022-07-18 00:29:57 +0900

    [update] auth getters 에서 localStorage 체크하도록 수정

--- src/store/auth/getters.js
+++ src/store/auth/getters.js
@@ -1,3 +1,20 @@
+import { LocalStorage } from 'quasar'
+
 export function isLoggedIn(state) {
-  return state.user != null
+  if (LocalStorage.getItem('auth')) {
+    // 유저정보 남아있는 상태
+    return true
+  } else {
+    // 유저정보 날라간 상태
+    return false
+  }
+}
+
+export function userInfo(state) {
+  const userInfo = LocalStorage.getItem('auth')
+  if (userInfo) {
+    return userInfo
+  } else {
+    return null
+  }
 }
