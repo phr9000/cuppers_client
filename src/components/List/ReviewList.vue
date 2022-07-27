7dd1391050dcef9be4e7a9c8433416eecd40b9cd
aestas <hawhawba@gmail.com>
2022-07-27 18:47:27 +0900

    [~ing] 리뷰 메인, 리뷰리스트 작업

--- src/components/List/ReviewList.vue
+++ src/components/List/ReviewList.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="review_list">
-    <div class="left">
-      <!-- <card-review :review=""></card-review> -->
-      <card-review></card-review>
+    <div class="whole">
+      <div class="cards_wrap" v-for="review in reviews" :key="review.review_id">
+        <card-review :review="review" />
+      </div>
     </div>
-    <div class="right"></div>
+    <div class="lr">
+      <div class="left">
+        <div class="" v-for="review in leftList" :key="review.review_id">
+          <card-review :review="review" :borderd="true" :flat="true" />
+        </div>
+      </div>
+      <div class="right">
+        <div class="" v-for="review in rightList" :key="review.review_id">
+          <card-review :review="review" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +29,7 @@ export default defineComponent({
   name: 'ReviewList',
   components: { CardReview },
   props: {
-    cnotes: {
+    reviews: {
       type: Array,
       default: () => {
         return null
@@ -27,13 +39,40 @@ export default defineComponent({
   data() {
     return {}
   },
+  computed: {
+    leftList() {
+      return this.reviews.filter((item, i) => {
+        return i % 2 === 0
+      })
+    },
+    rightList() {
+      return this.reviews.filter((item, i) => {
+        return i % 2 === 1
+      })
+    }
+  },
   created() {},
   methods: {}
 })
 </script>
 <style lang="scss" scoped>
 .review_list {
-  background-color: antiquewhite;
   width: 100%;
+  .whole {
+    display: none;
+    width: 100%;
+    background-color: $s-teal;
+  }
+  .lr {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    .left {
+      padding-right: 4px;
+    }
+    .right {
+      padding-left: 4px;
+    }
+  }
 }
 </style>
--- src/pages/ReviewMainPage.vue
+++ src/pages/ReviewMainPage.vue
@@ -12,19 +12,28 @@
             dense
             class="text-grey-6"
           >
-            <modal-search />
-            <q-tab class="btn_sort" name="like" label="추천순" />
-            <q-tab class="btn_sort" name="recent" label="최신순" />
+            <!-- 커핑노트 검색 -->
+            <search-toggle placeholder="커핑노트 검색" @search="handleSearch" />
+
+            <!-- 정렬 : 추천순 / 최신순 -->
+            <tab-sort
+              :sort_items="sortItems"
+              @change="changeSort"
+              style="height: 40px"
+            />
           </q-tabs>
         </div>
       </div>
     </div>
 
+    <!-- main -->
     <main
       class="review_main q-gutter-md constrain_md row justify-between align-start q-pa-md"
     >
       <div style="margin: 0">
-        <review-list />
+        <!-- {{ reviews }} -->
+        <review-list :reviews="reviews" />
+        <inf-scroll @entry="loadReviews" ref="infScroll" />
       </div>
     </main>
   </q-page>
@@ -35,12 +44,14 @@ import { defineComponent } from 'vue'
 import { computed } from 'vue'
 import { useStore } from 'vuex'
 
-import ModalSearch from 'src/components/Modal/ModalSearch.vue'
 import ReviewList from 'src/components/List/ReviewList.vue'
+import TabSort from 'src/components/Tab/TabSort.vue'
+import SearchToggle from 'src/components/Input/SearchToggle.vue'
+import InfScroll from 'src/components/Scroll/InfScroll.vue'
 
 export default defineComponent({
   name: 'ReviewMainPage',
-  components: { ModalSearch, ReviewList },
+  components: { ReviewList, TabSort, SearchToggle, InfScroll },
   setup() {
     const $store = useStore()
 
@@ -54,15 +65,72 @@ export default defineComponent({
   },
   data() {
     return {
+      reviews: [],
       sort: null,
       page: 0,
       limit: 10,
       order: 'recent', // 'recent' or 'like'
-      search: '커'
+      search: '',
+      end: false, // inf scroll end
+      sortItems: [
+        {
+          label: '추천순',
+          val: 'like'
+        },
+        {
+          label: '최신순',
+          val: 'recent'
+        },
+        { label: '거리순', val: 'distance' }
+      ]
     }
   },
-  mounted() {},
-  methods: {}
+  mounted() {
+    this.loadReviews()
+  },
+  methods: {
+    loadReviews() {
+      this.page++
+
+      let apiUrl = `${process.env.API}/review/1?page=${this.page}&limit=${this.limit}&order=${this.order}&search=${this.search}`
+      if (this.user) {
+        apiUrl = `${apiUrl}&user_id=${this.user.uid}`
+      }
+      this.$axios
+        .get(apiUrl)
+        .then((result) => {
+          console.log(result.data)
+          if (result.data.arr.length < 1) {
+            this.$refs.infScroll.stop()
+            return
+          }
+          result.data.arr.forEach((item) => {
+            this.reviews.push(item)
+          })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    handleSearch(search) {
+      // initList
+      this.reviews = []
+      this.page = 0
+      this.search = search
+      this.end = false
+
+      this.loadCnotes()
+    },
+    changeSort(val) {
+      console.log('chagne sort to: ', val)
+      this.order = val
+
+      this.reviews = []
+      this.page = 0
+      this.end = false
+      this.loadCnotes()
+    }
+  }
 })
 </script>
 <style lang="scss" scoped>
@@ -78,8 +146,6 @@ export default defineComponent({
     }
   }
   .review_main {
-    display: grid;
-    grid-template-columns: 1fr 224px;
     // padding: 0 12px;
     @media (max-width: $breakpoint-sm-max) {
       display: block;
